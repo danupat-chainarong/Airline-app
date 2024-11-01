0fa6ea97bc489c238317103ae5e0f8dbb03bd39b
Danupat Chainarong <125113738+danupat-chainarong@users.noreply.github.com>
2024-11-01 13:02:07 +0700

    fix app template

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { initializeApp } from 'firebase/app';
+import {EXPO_PUBLIC_API_KEY} from '@env';
+const apiKey = process.env.EXPO_PUBLIC_API_KEY;
 
-export default function App() {
+const firebaseConfig = {
+  // Add your own Firebase configuration
+  apiKey: apiKey,
+  authDomain: "flight-app-mobile-app-project.firebaseapp.com",
+  databaseURL: "https://flight-app-mobile-app-project-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "flight-app-mobile-app-project",
+  storageBucket: "flight-app-mobile-app-project.firebasestorage.app",
+  messagingSenderId: "584238601229",
+  appId: "1:584238601229:web:9ff4c869a8c1c747ea7144"
+};
+
+// Initialize Firebase app
+initializeApp(firebaseConfig);
+
+
+
+const App: React.FC = () => {
+  // ADD usefull func
+  console.log("test");
+  console.log(EXPO_PUBLIC_API_KEY);
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -9,6 +31,7 @@ export default function App() {
     </View>
   );
 }
+export default App;
 
 const styles = StyleSheet.create({
   container: {
